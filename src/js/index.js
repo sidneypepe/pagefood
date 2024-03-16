fc66cf8aa45bc109a9361e5b1e43076ecdd4b1ef
sidney <sidneypepe60@gmail.com>
2024-03-16 15:58:03 -0300

    menu-img

---
+++ src/js/index.js
@@ -0,0 +1,8 @@
+
+$(document).ready(function() {
+    $('#mobile-btn').on('click', function () {
+        $('#mobile-menu').toggleClass('active');
+        $('#mobile-btn').find('i').toggleClass('fa-x');
+});
+});
+
